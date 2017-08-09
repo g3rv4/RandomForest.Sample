f14133be359183f630536a12b51403c3b2755fb2
Gervasio Marchand <gmarchand@stackoverflow.com>
2017-08-08 21:07:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+RandomForest.Sample
+===
+Ever wondered how to use [RandomForest](https://github.com/g3rv4/RandomForest) but were afraid to ask? this is for you! ([the code you want is here](https://github.com/g3rv4/RandomForest.Sample/blob/master/RandomForest.Sample/Program.cs))
+
+I generated a random forest for regressions using [caret](http://caret.r-forge.r-project.org/) in [R](https://www.r-project.org/). You can see [the R code to generate and export the model here](https://github.com/g3rv4/RandomForests/blob/master/RegressionRandomForest.Rmd). It's predicting the final grade of math students given a bunch of variables (the data was grabbed from the [UCI Machine Learning Repository](http://archive.ics.uci.edu/ml/datasets/Student+Performance)).
+
+I then generated and exported the PMML file. I also used the model in R to predict the values for all the lines.
+
+Lastly, I ran the predictions in C# and compared them with the ones generated in R. This project is doing exactly that.
+
+If there's a difference bigger than 0.001%, it outputs "OOOOOPSSSSS". You're welcome to run this and verify it's *100% OOOOOPSSSSS free*.
---
+++ RandomForest.Sample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.IO;
+using CsvHelper;
+using System.Linq;
+
+namespace RandomForest.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forest randomForest;
+            using (XmlReader reader = XmlReader.Create("regression_rf.pmml"))
+            {
+                randomForest = new Forest(reader);
+            }
+
+            // we have the random forest! now let's build a dictionary to compare the predictions in R with the ones here
+            using (TextReader reader = File.OpenText("regression_rf_prediction.csv"))
+            {
+                // the whole point of this is to build a dictionary with the keys and the values from the csv
+                var csv = new CsvParser(reader);
+                string[] headers = csv.Read();
+                int lineNumber = 1;
+                while (true)
+                {
+                    var row = csv.Read();
+                    if(row == null)
+                    {
+                        break;
+                    }
+
+                    var item = headers.Zip(row, (key, value) => new { key, value }).ToDictionary(e => e.key, e => e.value);
+
+                    var rPrediction = double.Parse(item["PredictedG3"]);
+                    var cSharpPrediction = randomForest.Predict(item);
+
+                    Console.WriteLine($"Processing line {lineNumber++} - R prediction: {rPrediction} - C#: {cSharpPrediction}");
+
+                    var differencePercentage = Math.Abs((rPrediction - cSharpPrediction) / rPrediction);
+                    if(differencePercentage >= 0.00001)
+                    {
+                        Console.WriteLine("   OOOOOPSSSSS");
+                    }
+                }
+            }
+        }
+    }
+}
